da42027ee81ddbd6292bb226291badf451b73669
Fernando Leyva <fernando_rendel892@hotmail.com>
2023-07-25 17:55:36 -0600

    feat(points-caculate-angle):se implementó la función para detectar los puntos cercanos a los bordes de la figura, finalmente se está detectando en una primera version los rectangulos que estan forman

--- calc_angle.py
+++ calc_angle.py
@@ -19,7 +19,9 @@ image = cv2.imread('img/Captura de pantalla (164).png')
 
 # Definir los rangos de color para los colores que deseas conservar
 selected_color_ranges = [
-    [(170, 130, 200), (250, 255, 255)]
+    [(170, 130, 200), (250, 255, 255)],
+    [(160, 100, 100), (180, 255, 255)],
+    [(0, 100, 100), (10, 255, 255)]
 ]
 
 # Conservar solo los colores seleccionados en la imagen
@@ -34,47 +36,57 @@ edges = cv2.Canny(gray_image, threshold1=30, threshold2=100)
 # Encontrar los contornos en la imagen binaria
 contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+lista_x1 = []
+
 # Trazar líneas verticales y horizontales
 for contour in contours:
-    vertical_lines_l = cv2.HoughLinesP(edges, rho=1, theta=np.pi/2, threshold=1, minLineLength=40, maxLineGap=1)
+    '''vertical_lines_l = cv2.HoughLinesP(edges, rho=1, theta=np.pi/2, threshold=1, minLineLength=40, maxLineGap=1)
     if vertical_lines_l is not None:
         for line in vertical_lines_l:
             x1, y1, x2, y2 = line[0]
             # Puntos de inicio y fin en el borde superior e inferior de la imagen
             cv2.line(image_with_selected_colors, (x1, 0), (x2, image_with_selected_colors.shape[0]), (0, 0, 255), 2)
-
-    vertical_lines_r = cv2.HoughLinesP(edges, rho=1, theta=np.pi/2, threshold=1, minLineLength=30, maxLineGap=1)
+            cv2.putText(image_with_selected_colors, f"x1_lvl: {x1}", (x1, x1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            cv2.putText(image_with_selected_colors, f"x2_lvl: {x2}", (x2, x2), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            cv2.putText(image_with_selected_colors, f"y1_lvl: {y1}", (y1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            cv2.putText(image_with_selected_colors, f"y2_lvl: {y2}", (y2, y2), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+    '''
+    vertical_lines_r = cv2.HoughLinesP(edges, rho=1, theta=np.pi/2, threshold=1, minLineLength=1, maxLineGap=1)
     if vertical_lines_r is not None:
         for line in vertical_lines_r:
             x1, y1, x2, y2 = line[0]
+            cv2.putText(image_with_selected_colors, f"{x1}", (x1, y2), cv2.FONT_HERSHEY_SIMPLEX, 0.1, (0, 255, 255), 2)
+
+            top_left = (x1, y2)
+            bottom_right = (x1 + 50, y2 + 100)  # Puedes ajustar los valores para el tamaño del rectángulo
+
+            # Dibujar el rectángulo en la imagen
+            cv2.rectangle(image_with_selected_colors, top_left, bottom_right, (0, 0, 255), 2)
+
+            #cv2.putText(image_with_selected_colors, f"{x1}", (x1, y2), cv2.FONT_HERSHEY_SIMPLEX, 0.1, (0, 255, 255), 2)
             # Puntos de inicio y fin en el borde superior e inferior de la imagen
-            cv2.line(image_with_selected_colors, (x1, 0), (x2, image_with_selected_colors.shape[1]), (0, 0, 255), 2)
-    
+            #cv2.line(image_with_selected_colors, (x1, y2), (x2, y2), (0, 0, 255), 2)
+            #vertical_line = f"Linea vertical"
+            #cv2.putText(image_with_selected_colors, f"{x1}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (255, 255, 0), 2)
+            #cv2.line(image_with_selected_colors, (x1, y2), (x1, y1), (0, 0, 255), 1)
+            #cv2.putText(image_with_selected_colors, f"y1_lvr: {y1}", (y1, x1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            #cv2.putText(image_with_selected_colors, f"y2_lvr: {y2}", (y2, x2), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+
+    '''
     horizontal_lines_l = cv2.HoughLinesP(edges, rho=1, theta=np.pi/2, threshold=1, minLineLength=22, maxLineGap=1)
     if horizontal_lines_l is not None:
         for line in horizontal_lines_l:
             x1, y1, x2, y2 = line[0]
             # Puntos de inicio y fin en el borde superior e inferior de la imagen
-            cv2.line(image_with_selected_colors, (0, y1), (image_with_selected_colors.shape[0], y2), (0, 255, 0), 2)
+            cv2.line(image_with_selected_colors, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(image_with_selected_colors, f"x1_lhl: {x1}", (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(image_with_selected_colors, f"x2_lhl: {x2}", (x2, y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(image_with_selected_colors, f"y1_lhl: {y1}", (y1, x1), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(image_with_selected_colors, f"y2_lhl: {y2}", (y2, x2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    '''
 
-if horizontal_lines_l is not None and vertical_lines_l is not None:
-    for hl in horizontal_lines_l:
-        for vl in vertical_lines_l:
-            x1_h, y1_h, x2_h, y2_h = hl[0]
-            x1_v, y1_v, x2_v, y2_v = vl[0]
-
-            dx = x2_h - x1_h
-            dy = y2_v - y1_v
-
-            # Calcular el ángulo en radianes
-            angle_radians = np.arctan2(dy, dx)
-
-            # Convertir el ángulo a grados
-            angle_degrees = np.degrees(angle_radians)
-
-            angle_text = f"Angulo: {angle_degrees:.2f} grados"
-            cv2.putText(image_with_selected_colors, angle_text, (x1_h, y1_h), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
+lista_x1.sort()
+print(lista_x1)
 
 window_width = 1980
 window_height = 720
